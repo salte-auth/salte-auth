ccd9d80f64c0103301b57a86917aeec9c4c8c9dd
Nick Woodward <nick@salte.io>
2016-10-20 22:22:56 -0500

    fix: remove the `_decode` function (#18)
    This drops support for the following browsers:
    
    * IE 9 and lower

--- src/salte-auth.js
+++ src/salte-auth.js
@@ -949,59 +949,8 @@ export default class AuthenticationContext {
   }
 
   _base64DecodeStringUrlSafe(base64IdToken) {
-    // html5 should support atob function for decoding
     base64IdToken = base64IdToken.replace(/-/g, '+').replace(/_/g, '/');
-    if (window.atob) {
-      return decodeURIComponent(escape(window.atob(base64IdToken)));
-    }
-    return decodeURIComponent(escape(this._decode(base64IdToken)));
-  }
-
-  // Take https://cdnjs.cloudflare.com/ajax/libs/Base64/0.3.0/base64.js and https://en.wikipedia.org/wiki/Base64 as reference.
-  _decode(base64IdToken) {
-    const codes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-    base64IdToken = String(base64IdToken).replace(/[=]+$/, '');
-
-    const length = base64IdToken.length;
-    if (length % 4 === 1) {
-      throw new Error('The token to be decoded is not correctly encoded.');
-    }
-
-    let h1, h2, h3, h4, bits, c1, c2, c3;
-    let decoded = '';
-    for (let i = 0; i < length; i += 4) {
-      // Every 4 base64 encoded character will be converted to 3 byte string, which is 24 bits
-      // then 6 bits per base64 encoded character
-      h1 = codes.indexOf(base64IdToken.charAt(i));
-      h2 = codes.indexOf(base64IdToken.charAt(i + 1));
-      h3 = codes.indexOf(base64IdToken.charAt(i + 2));
-      h4 = codes.indexOf(base64IdToken.charAt(i + 3));
-
-      // For padding, if last two are '='
-      if (i + 2 === length - 1) {
-        bits = h1 << 18 | h2 << 12 | h3 << 6;
-        c1 = bits >> 16 & 255;
-        c2 = bits >> 8 & 255;
-        decoded += String.fromCharCode(c1, c2);
-        break;
-      } else if (i + 1 === length - 1) { // if last one is '='
-        bits = h1 << 18 | h2 << 12;
-        c1 = bits >> 16 & 255;
-        decoded += String.fromCharCode(c1);
-        break;
-      }
-
-      bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
-
-      // then convert to 3 byte chars
-      c1 = bits >> 16 & 255;
-      c2 = bits >> 8 & 255;
-      c3 = bits & 255;
-
-      decoded += String.fromCharCode(c1, c2, c3);
-    }
-
-    return decoded;
+    return decodeURIComponent(escape(window.atob(base64IdToken)));
   }
 
     // Auth.node js crack function
