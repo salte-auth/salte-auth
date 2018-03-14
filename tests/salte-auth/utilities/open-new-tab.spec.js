c37e701ebe6f96c29dcb88a655fdea39c67ae98a
Nick Woodward <nick@salte.io>
2018-03-14 10:29:27 -0500

    tests: `openNewTab` should name the tab `salte-auth`

--- tests/salte-auth/utilities/open-new-tab.spec.js
+++ tests/salte-auth/utilities/open-new-tab.spec.js
@@ -14,15 +14,18 @@ describe('function(openNewTab)', () => {
   });
 
   it('should open a new tab', () => {
-    sandbox.stub(window, 'open').returns({
+    const newTab = {
       closed: false,
       focus: sandbox.stub(),
       close: function() {
         this.closed = true;
       }
-    });
+    };
+    sandbox.stub(window, 'open').returns(newTab);
 
     const promise = utilities.openNewTab('https://www.google.com');
+    
+    expect(newTab.name).to.equal('salte-auth');
 
     setTimeout(() => {
       window.open.firstCall.returnValue.close();
