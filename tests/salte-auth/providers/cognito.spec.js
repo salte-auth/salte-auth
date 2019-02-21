35e20629bf7812a40fd622cc1c3316cb84e49057
Ceci <admin@cecilias.me>
2019-02-20 19:19:18 -0600

    feat: added okta to the list of supported providers (#326)
    closes #97

--- tests/salte-auth/providers/cognito.spec.js
+++ tests/salte-auth/providers/cognito.spec.js
@@ -8,31 +8,39 @@ describe('cognito', () => {
 
   describe('function(authorizeEndpoint)', () => {
     it('should create a authorize endpoint', () => {
-      expect(cognito.authorizeEndpoint.call({ $utilities: utilities }, {
+      const url = new URL(cognito.authorizeEndpoint.call({ $utilities: utilities }, {
         providerUrl: 'https://mydomain.auth.us-east-1.amazoncognito.com'
-      })).to.equal(`https://mydomain.auth.us-east-1.amazoncognito.com/oauth2/authorize`);
+      }));
+
+      expect(url.origin + url.pathname).to.equal('https://mydomain.auth.us-east-1.amazoncognito.com/oauth2/authorize');
     });
   });
 
   describe('function(deauthorizeUrl)', () => {
     it('should create a logout url', () => {
-      const url = cognito.deauthorizeUrl.call({ $utilities: utilities }, {
+      const url = new URL(cognito.deauthorizeUrl.call({ $utilities: utilities }, {
         providerUrl: 'https://mydomain.auth.us-east-1.amazoncognito.com',
         redirectUrl: `${location.protocol}//${location.host}`,
         clientId: '33333333-3333-4333-b333-333333333333'
-      });
-      expect(url).to.equal(`https://mydomain.auth.us-east-1.amazoncognito.com/logout?logout_uri=${encodeURIComponent(`${location.protocol}//${location.host}`)}&client_id=33333333-3333-4333-b333-333333333333`);
+      }));
+
+      expect(url.origin + url.pathname).to.equal('https://mydomain.auth.us-east-1.amazoncognito.com/logout');
+      expect(url.searchParams.get('client_id')).to.equal('33333333-3333-4333-b333-333333333333');
+      expect(url.searchParams.get('logout_uri')).to.equal(`${location.protocol}//${location.host}`);
     });
 
     it('should support a separate logoutUrl', () => {
-      const url = cognito.deauthorizeUrl.call({ $utilities: utilities }, {
+      const url = new URL(cognito.deauthorizeUrl.call({ $utilities: utilities }, {
         providerUrl: 'https://mydomain.auth.us-east-1.amazoncognito.com',
         redirectUrl: {
           logoutUrl: `${location.protocol}//${location.host}`
         },
         clientId: '33333333-3333-4333-b333-333333333333'
-      });
-      expect(url).to.equal(`https://mydomain.auth.us-east-1.amazoncognito.com/logout?logout_uri=${encodeURIComponent(`${location.protocol}//${location.host}`)}&client_id=33333333-3333-4333-b333-333333333333`);
+      }));
+
+      expect(url.origin + url.pathname).to.equal('https://mydomain.auth.us-east-1.amazoncognito.com/logout');
+      expect(url.searchParams.get('client_id')).to.equal('33333333-3333-4333-b333-333333333333');
+      expect(url.searchParams.get('logout_uri')).to.equal(`${location.protocol}//${location.host}`);
     });
   });
 
